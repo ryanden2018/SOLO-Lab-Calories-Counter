23afc9a9cb118fb0d7ce2fc12af2b6f6229a670a
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-23 11:31:42 -0500

    done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const divDropDown = document.querySelector('.dropdown')
     const divUserInfo = document.querySelector('.userInfo')
     const selectTag = document.createElement('select')
+    const createUserForm = document.querySelector('#form-create-user');
 
     let opTitle = document.createElement('option')
     opTitle.innerText = "Select a User"
@@ -15,15 +16,86 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fetchingUsers(){
         //request dara from server and call renderUser function for each user
+        users = []
+        selectTag.innerHTML = ""
+        let opTitle = document.createElement('option')
+        opTitle.innerText = "Select a User"
+        opTitle.selected = true
+        opTitle.disabled = true
+        selectTag.append(opTitle)
+        fetch(usersURL)
+        .then( res => res.json() )
+        .then( data => {
+            data.forEach( elem => {
+                let user = new UserInfo(elem.id,elem.name,elem.image,elem.calories.total_calories);
+                users.push(user);
+                renderUser(user);
+            });
+        });
     }
 
     function renderUser(user){
         // creating and appending options to the selectTag for each User
+        let op = document.createElement("option");
+        op.innerText = user.name;
+        selectTag.append(op);
     }
 
     selectTag.addEventListener('change', (e) => {
         //find a user and Use userInfo class to show user Info inside 'user-info' div 
+        let user = users.find( user => user.name === e.target.value );
+        UserInfo.renderUserInfo(user);
     })
+
+    divUserInfo.addEventListener("click", e => {
+
+        if(e.target.tagName === "BUTTON") { // reset
+            let user = users.find( 
+                user => user.name === 
+                  e.target.parentElement.querySelector("p").innerText
+            );
+            user.resetCalories(usersURL);
+            e.target.parentElement.querySelector("h4").innerText = `Total Calories: ${user.calories.total_calories}`;
+        }
+
+        if((e.target.tagName === "INPUT") && (e.target.getAttribute("type")==="submit")) {
+            e.preventDefault();
+            let user = users.find( 
+                user => user.name === 
+                  e.target.parentElement.parentElement.querySelector("p").innerText
+            );
+            if(!Number.isNaN(e.target.value)) {
+                user.addCalories(parseInt(e.target.parentElement.querySelector("input[type=text]").value),usersURL);
+            }
+            e.target.parentElement.parentElement.querySelector("h4").innerText = `Total Calories: ${user.calories.total_calories}`;
+            e.target.parentElement.reset();
+        }
+
+        
+
+        
+    });
+
+    createUserForm.addEventListener("submit", e => {
+        e.preventDefault();
+        let nameInput = createUserForm.querySelector("#text-input-name");
+        let urlInput = createUserForm.querySelector("#text-input-url");
+        fetch(usersURL, {
+            method:"POST",
+            headers:{ "Content-Type":"application/json",
+                      "Accept":"application/json"},
+            body: JSON.stringify({
+                name:nameInput.value,
+                image:urlInput.value,
+                calories: { total_calories: 0 }
+            })
+        })
+        .then( res => res.json() )
+        .then( data => {
+            fetchingUsers();
+        });
+        createUserForm.reset();
+    });
 
     divDropDown.append(selectTag)
     fetchingUsers()
--- src/userInfo.js
+++ src/userInfo.js
@@ -1,8 +1,53 @@
 class UserInfo{
+    constructor(id,name,image,totalCalories) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.calories = new Calories(totalCalories);
+    }
    
-    renderUserInfo(user){
-        
+    static renderUserInfo(user){
+        let divUserInfo = document.querySelector('.userInfo');
+        divUserInfo.querySelectorAll("div").forEach( div =>
+            div.style.display = "none"
+        );
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        let img = document.createElement("img");
+        let h4 = document.createElement("h4");
+        let form = document.createElement("form");
+        let textInput = document.createElement("input");
+        textInput.setAttribute("type","text");
+        let submitInput = document.createElement("input");
+        submitInput.setAttribute("type","submit");
+        let btn = document.createElement("button");
+        p.innerText = user.name;
+        img.src = user.image;
+        h4.innerText = `Total Calories: ${user.calories.total_calories}`;
+        textInput.placeholder =  "Enter Calories";
+        submitInput.value = "Add Calories";
+        form.append(textInput,submitInput);
+        btn.innerText = "Reset Calories";
+        div.append(p,img,h4,form,btn);
+        divUserInfo.append(div);
     }
 
+    resetCalories(URL) {
+        this.addCalories((-1)*this.calories.total_calories,URL);
+    }
 
+    addCalories(extraCalories,URL) {
+        this.calories.total_calories += extraCalories
+
+        fetch(`${URL}/${this.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type":"application/json",
+                      "Accept":"application/json"},
+            body: JSON.stringify(
+                {
+                    calories: { total_calories: this.calories.total_calories }
+                }
+            )
+        })
+    }
 }
